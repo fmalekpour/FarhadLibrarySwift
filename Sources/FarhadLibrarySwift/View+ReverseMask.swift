1312aaed318e57ff75c0060bdadcf2db1f32e96a
fmalekpour <fm@farhad.ca>
2023-12-17 17:30:03 -0800

    fx

--- Sources/FarhadLibrarySwift/View+ReverseMask.swift
+++ Sources/FarhadLibrarySwift/View+ReverseMask.swift
@@ -1,8 +1,26 @@
 //
-//  File.swift
+//  View+ReverseMask.swift
 //  
 //
 //  Created by Farhad Malekpour on 12/17/23.
 //
 
 import Foundation
+import SwiftUI
+
+@available(iOS 15.0, *)
+extension View {
+	@inlinable
+	public func fmReverseMask<Mask: View>(
+		alignment: Alignment = .center,
+		@ViewBuilder _ mask: () -> Mask
+	) -> some View {
+		self.mask {
+			Rectangle()
+				.overlay(alignment: alignment) {
+					mask()
+						.blendMode(.destinationOut)
+				}
+		}
+	}
+}
