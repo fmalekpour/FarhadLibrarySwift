bfd0ddc47b05aea5cec99b662ae86da2cd0a5abe
fmalekpour <fm@farhad.ca>
2023-12-21 00:25:21 -0800

    --

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "FarhadLibrarySwift",
+	platforms: [
+		.iOS(.v13),         
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v10),
+	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
