555121a11ed858ac452b4e8582c58abdd0b47851
fmalekpour <fm@farhad.ca>
2023-12-21 00:34:52 -0800

    Color

--- Sources/FarhadLibrarySwift/Color/Color+Extensions.swift
+++ Sources/FarhadLibrarySwift/Color/Color+Extensions.swift
@@ -1,8 +1,19 @@
 //
-//  File.swift
-//  
+//  Color+Extensions.swift
+//
 //
 //  Created by Farhad Malekpour on 12/21/23.
 //
 
 import Foundation
+import SwiftUI
+
+extension Color
+{
+	public static var faRandom: Color { Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1)) }
+}
+
+extension ShapeStyle where Self == Color
+{
+	public static var faRandom: Color { Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1)) }
+}
