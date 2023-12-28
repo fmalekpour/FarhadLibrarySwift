8b378bf1d13feee800f430cecbc46e257168750d
fmalekpour <fm@farhad.ca>
2023-12-28 10:06:34 -0800

    Changed to unnamed path

--- Sources/FarhadLibrarySwift/T4/FilePath.swift
+++ Sources/FarhadLibrarySwift/T4/FilePath.swift
@@ -9,31 +9,31 @@ import Foundation
 
 public extension T4
 {
-	static func DOCUMENTS_PATH(path: String) -> String?
+	static func DOCUMENTS_PATH(_ path: String) -> String?
 	{
 		guard let st1 = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else { return nil }
 		return String(NSString(string: st1).appendingPathComponent(path))
 	}
 	
-	static func LIBRARY_PATH(path: String) -> String?
+	static func LIBRARY_PATH(_ path: String) -> String?
 	{
 		guard let st1 = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first else { return nil }
 		return String(NSString(string: st1).appendingPathComponent(path))
 	}
 	
-	static func CACHES_PATH(path: String) -> String?
+	static func CACHES_PATH(_ path: String) -> String?
 	{
 		guard let st1 = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else { return nil }
 		return String(NSString(string: st1).appendingPathComponent(path))
 	}
 	
-	static func BUNDLE_PATH(path: String) -> String
+	static func BUNDLE_PATH(_ path: String) -> String
 	{
 		let st1 = Bundle.main.bundlePath
 		return String(NSString(string: st1).appendingPathComponent(path))
 	}
 	
-	static func APPLICATION_SUPPORT_PATH(path: String) -> String?
+	static func APPLICATION_SUPPORT_PATH(_ path: String) -> String?
 	{
 		guard let st1 = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true).first else { return nil }
 		return String(NSString(string: st1).appendingPathComponent(path))
