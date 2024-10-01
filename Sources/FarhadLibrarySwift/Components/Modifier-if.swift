eab4ae9686648ebe69f3f26beb37c86c2a9088ee
fmalekpour <fm@farhad.ca>
2024-10-01 10:26:14 -0700

    SwiftUI - View - Modify

--- Sources/FarhadLibrarySwift/Components/Modifier-if.swift
+++ Sources/FarhadLibrarySwift/Components/Modifier-if.swift
@@ -22,4 +22,14 @@ public extension View {
 			self
 		}
 	}
+	
+	@ViewBuilder
+	func modify(@ViewBuilder _ transform: (Self) -> (some View)?) -> some View {
+		if let view = transform(self), !(view is EmptyView) {
+			view
+		} else {
+			self
+		}
+	}
+
 }
