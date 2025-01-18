7a3fb9a9adb6b3189d99e77567db114b54d37adc
fmalekpour <fm@farhad.ca>
2025-01-18 12:34:18 -0800

    View+Size

--- Sources/FarhadLibrarySwift/SwiftUI/View+Size.swift
+++ Sources/FarhadLibrarySwift/SwiftUI/View+Size.swift
@@ -11,12 +11,30 @@ import SwiftUI
 @available(iOS 17.0, *)
 private struct FMReportSize: ViewModifier {
 	var mReporter: (_ size: CGSize) -> Void = {_ in}
+	var mInitial: Bool
 	func body(content: Content) -> some View {
 		content
 			.overlay {
 				GeometryReader { geo in
 					Color.clear
-						.onChange(of: geo.size, initial: true) { oldValue, newValue in
+						.onChange(of: geo.size, initial: mInitial) { oldValue, newValue in
+							mReporter(newValue)
+						}
+				}
+			}
+	}
+}
+
+@available(iOS 17.0, *)
+private struct FMReportSafeAreaInsets: ViewModifier {
+	var mReporter: (_ insets: EdgeInsets) -> Void = {_ in}
+	var mInitial: Bool
+	func body(content: Content) -> some View {
+		content
+			.overlay {
+				GeometryReader { geo in
+					Color.clear
+						.onChange(of: geo.safeAreaInsets, initial: mInitial) { oldValue, newValue in
 							mReporter(newValue)
 						}
 				}
@@ -27,8 +45,14 @@ private struct FMReportSize: ViewModifier {
 
 public extension View {
 	@available(iOS 17.0, *)
-	func fmReportSize(_ reporter: @escaping (_ size: CGSize) -> Void) -> some View {
-		modifier(FMReportSize(mReporter: reporter))
+	func fmReportSize(initial: Bool = true, _ reporter: @escaping (_ size: CGSize) -> Void) -> some View {
+		modifier(FMReportSize(mReporter: reporter, mInitial: initial))
 	}
+
+	@available(iOS 17.0, *)
+	func fmReportSafeAreaInsets(initial: Bool = true, _ reporter: @escaping (_ insets: EdgeInsets) -> Void) -> some View {
+		modifier(FMReportSafeAreaInsets(mReporter: reporter, mInitial: initial))
+	}
+
 }
 
